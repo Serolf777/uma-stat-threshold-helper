1157b604df5b740176e3685c2c184ed9c312b984
Serolf777 <kookie-serolf@hotmail.com>
2026-01-13 15:08:01 -0500

    updating to hashrouter

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, HashRouter } from "react-router-dom";
 import MainPage from "./main-page/MainPage.tsx";
 import './App.scss';
 
 export default function App() {
     return (
         <div className="App">
-          <Routes>
-              <Route path="/" element={<MainPage />} />
-          </Routes>
+            <HashRouter>
+                <Routes>
+                    <Route path="/" element={<MainPage />} />
+                </Routes>
+          </HashRouter>
         </div>
     )
 };
--- src/index.js
+++ src/index.js
@@ -2,14 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.js';
 import reportWebVitals from './reportWebVitals.js';
-import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/uma-stat-threshold-helper">
       <App />
-    </BrowserRouter>
   </React.StrictMode>
 );
 
